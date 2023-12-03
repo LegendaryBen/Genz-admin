b87a5dbe9d89f7400b1cd777eefc4153bd9b7207
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2023-12-03 13:49:24 +0000

    added some scripts to the authors_card component

---
+++ src/Components/Authors_card.jsx
@@ -0,0 +1,15 @@
+import chima from "../Images/chima.jpg"
+
+const Authors_card = () => {
+    return (
+        <div className="authors_card">
+            <div>
+                <span className="material-symbols-outlined">person</span>
+                <div>Benjamin Chimaobi</div>
+            </div>
+            <span className="material-symbols-outlined">navigate_next</span>
+        </div>
+    )
+}
+
+export default Authors_card
--- src/Components/Home_first.jsx
+++ src/Components/Home_first.jsx
@@ -1,9 +1,22 @@
 import Notify_Block from "./Notify_Block"
+import { Link } from "react-router-dom"
+import Authors_card from "./Authors_card"
+import { useState } from "react"
 
 
 
 
 const Home_first = () => {
+
+    const[drop,setDrop] = useState(false)
+
+
+    const dropDown = ()=>{
+        setDrop(!drop);
+    }
+
+
+
     return (
         <div className='home_first'>
             <div className="home_details">
@@ -14,23 +27,28 @@ const Home_first = () => {
                 <Notify_Block image='person' total="Total Users" number={896896} monthly="All time increase"/>
                 <Notify_Block image='person' total="Total Users" number={896896} monthly="All time increase"/>
             </div>
-            <div className="add_authors">
+            <div className="add_authors" onClick={dropDown}>
                 <div className="authors_toggle">
                     <div className="toggle1">
                         <span className="material-symbols-outlined">group</span>
                         <div>Authors</div>
                     </div>
                     <div className="toggle2">
-                        <span className="material-symbols-outlined">keyboard_arrow_down</span>
+                        <span className="material-symbols-outlined" style={{transform:!drop ? "rotate(0deg)":"rotate(180deg)",transition:"0.5s"}}>keyboard_arrow_down</span>
                     </div>
                 </div>
-                <div className="show_authors">
-                    
+                <div className="show_authors" style={{height:!drop ? "0px":"300px"}}>
+                    <Authors_card/>
+                    <Authors_card/>
+                    <Authors_card/>
+                    <Authors_card/>
+                    <Authors_card/>
+                    <Authors_card/>
                 </div>
-                <div className="author_btn">
+                <Link className="author_btn">
                     <span className="material-symbols-outlined">add</span>
                     <div>Add new author</div>
-                </div>
+                </Link>
             </div>
         </div>
     )
